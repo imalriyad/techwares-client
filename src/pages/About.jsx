0757eedb52f70a1d0bc5ac27b294579a4d6004f3
imalriyad <imalriyad@gmail.com>
2023-10-18 03:11:56 +0600

    six brandName and image added in home route

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,38 @@
 
 const About = () => {
     return (
-        <div>
-            <h1>This is about page</h1>
+        <div className="max-w-screen-xl mx-auto mt-5 px-4 space-y-7">
+        
+<h1 className="md:text-3xl text-xl  font-bold">Welcome to TechWares – Your Premier Destination for Cutting-Edge Technology and Phones!</h1>
+
+<div>At TechWares, we are more than just a technology and phone shop; we are your trusted partner on the journey to discover and embrace the future. Since our inception, our mission has been to provide our customers with the latest and most innovative gadgets and devices, all while offering unparalleled service and support.
+</div>
+
+<div>
+<span className="md:text-3xl text-xl font-bold">Our Vision</span> <br /><br />
+
+Our vision is simple: to be the go-to destination for individuals, families, and businesses seeking top-of-the-line technology and phone solutions. We believe in a world where technology enriches our lives and connects us in ways we could have only imagined. We strive to be at the forefront of this digital revolution, making the future accessible today.
+
+</div>
+<div>
+
+<span className="md:text-3xl text-xl font-bold">Why Choose TechWares?</span> <br /><br />
+Unrivaled Product Selection: We curate an extensive inventory of the latest smartphones, tablets, laptops, and accessories. Our commitment to quality ensures that you only find the most trusted and reliable brands on our shelves.
+
+</div>
+<br /><br />
+
+<span className="md:text-3xl text-xl font-bold">Expert Guidance:</span><br /> <br />Our knowledgeable and friendly staff is here to assist you in finding the perfect device for your needs. Whether you're a tech enthusiast or a novice, we make sure you leave our store with the best fit for your lifestyle.
+<br />
+<br />
+<span className="md:text-3xl text-xl font-bold">Competitive Prices: </span> <br /> <br />We understand that quality technology doesn't have to come with an exorbitant price tag. We work hard to provide you with competitive prices and exclusive deals.
+
+
+Repair and Support Services: Life happens, and accidents can occur. Our in-house technicians are on hand to repair and support your devices, ensuring they stay in peak condition.
+
+Customer-Centric Approach: Your satisfaction is our priority. We're committed to building lasting relationships with our customers, providing a personalized and attentive shopping experience.
+
+Community Involvement: TechWares believes in giving back to the community that supports us. We actively engage in local initiatives and support causes that matter to us and our customers.
         </div>
     );
 };
