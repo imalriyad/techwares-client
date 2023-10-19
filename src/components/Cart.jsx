15d8effe134f77c97e51be49ebcab25f4d6a5e70
imalriyad <imalriyad@gmail.com>
2023-10-20 01:14:25 +0600

    delete confirmation message added

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,11 @@
 import { useLoaderData } from "react-router-dom";
 import Card from "../Brands/Card";
 import swal from "sweetalert";
+import { useState } from "react";
 
 const Cart = () => {
   const cartProduct = useLoaderData();
+  const [product, setProduct] = useState(cartProduct);
   const deletebtn = (id) => {
     swal({
       title: "Are you sure?",
@@ -24,6 +26,10 @@ const Cart = () => {
                 "Product successfully deleted form your cart!",
                 "success"
               );
+              const remainigProduct = product?.filter(
+                (item) => item._id !== id
+              );
+              setProduct(remainigProduct);
             }
           });
       }
@@ -32,10 +38,10 @@ const Cart = () => {
   return (
     <div>
       <h1 className="text-center font-bold text-4xl my-10">
-        Number of Carted product : {cartProduct.length}
+        Number of Carted product : {product?.length}
       </h1>
       <div className="mx-auto max-w-screen-2xl px-4 grid lg:grid-cols-3 md:grid-cols-2 gap-5 grid-cols-1">
-        {cartProduct?.map((item) => (
+        {product?.map((item) => (
           <Card key={item._id} deletebtn={deletebtn} item={item}></Card>
         ))}
       </div>
